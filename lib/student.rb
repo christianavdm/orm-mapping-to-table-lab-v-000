462fce29ef56a2223d2ee7aa85551f4152f8a08c
Christiana Vandermale <christianavdm@gmail.com>
2018-06-06 04:23:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,4 +40,10 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
   
+  def self.create(name:, album:)
+    song = Song.new(name, album)
+    song.save
+    song
+  end
+  
 end
